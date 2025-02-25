69808735bb080d4b1e8f86b93049845fd5069335
LoganLuo46 <limingluo2025@u.northwestern.edu>
2025-02-25 15:44:43 -0600

    add sorting from main

--- backend/dbFunctions.ts
+++ backend/dbFunctions.ts
@@ -1,5 +1,5 @@
 import db from './firebaseInit';
-import { collection, getDocs, addDoc, Timestamp } from 'firebase/firestore/lite';
+import { collection, getDocs, addDoc, Timestamp, query, orderBy } from 'firebase/firestore/lite';
 
 // type for journal entry
 interface Entry {
@@ -12,7 +12,12 @@ interface Entry {
 // output: array of journal entries [{timestamp: string, title: string, content: string}, ...]
 export async function getUserEntries(userId: string) {
     // Query a reference to a subcollection
-    const querySnapshot = await getDocs(collection(db, "users", userId, "journalEntries"));
+    const q = query(
+      collection(db, "users", userId, "journalEntries"),
+      orderBy("timestamp", "desc")
+    );
+
+    const querySnapshot = await getDocs(q);
     return querySnapshot.docs.map(doc => doc.data()) as Entry[];
 }
 
@@ -20,4 +25,5 @@ export async function getUserEntries(userId: string) {
 export async function postUserEntry(userId: string, entryData: Entry) {
     // Add a new document with a generated id.
     await addDoc(collection(db, "users", userId, "journalEntries"), entryData);
+    
 }
